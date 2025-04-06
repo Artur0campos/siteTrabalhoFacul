d35805ebf97d8012a6805284f5a5f72422bce935
Kauan Brandao <162450242+KauanBrandao@users.noreply.github.com>
2025-04-06 00:53:14 -0300

    a

--- app.py
+++ app.py
@@ -1,22 +1,44 @@
 from flask import Flask, render_template, request
 
+import matplotlib.pyplot as plt
+import math
+import os
+
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    resultado = None
-    mensagem = ""
-    if request.method == "POST":
-        nota1 = float(request.form.get("nota1", 0))
-        nota2 = float(request.form.get("nota2", 0))
-        resultado = (nota1 + nota2)/2
-        
-        if resultado >= 7 :
-            mensagem = "Parabens você passou!"
-        else:
-            mensagem = "te vejo na final!"
-    
-    return render_template("index.html", resultado=resultado, mensagem = mensagem)
+    if request.method == 'POST':
+        v0 = float(request.form['v0'])
+        angulo = float(request.form['angulo'])
+        y0 = float(request.form['y0'])
 
-if __name__ == "__main__":
+        theta = math.radians(angulo)
+        v0x = v0 * math.cos(theta)
+        v0y = v0 * math.sin(theta)
+        g = 9.8
+
+        t_total = (v0y + math.sqrt(v0y**2 + 2 * g * y0)) / g
+        tempos = [t / 100 for t in range(int(t_total * 100) + 1)]
+        x = [v0x * t for t in tempos]
+        y = [y0 + v0y * t - 0.5 * g * t**2 for t in tempos]
+
+        plt.figure(figsize=(10, 5))
+        plt.plot(x, y, color='green')
+        plt.title('Trajetória do Lançamento Oblíquo')
+        plt.xlabel('Distância (m)')
+        plt.ylabel('Altura (m)')
+        plt.grid(True)
+        plt.ylim(bottom=0)
+
+
+        caminho = os.path.join('static', 'trajeto.png')
+        plt.savefig(caminho)
+        plt.close()
+
+        return render_template('index.html', imagem=caminho)
+
+    return render_template('index.html', imagem=None)
+
+if __name__ == '__main__':
     app.run(debug=True)
